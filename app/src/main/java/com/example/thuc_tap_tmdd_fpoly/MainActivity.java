975d557e846a165b4950ec7a1b0d90f960ca90d5
thangPQPH27877 <thangpqph27877@fpt.edu.vn>
2023-11-18 04:08:18 +0700

    firt_commic

---
+++ app/src/main/java/com/example/thuc_tap_tmdd_fpoly/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.thuc_tap_tmdd_fpoly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Thuc_Tap_TMDD_FPOLY"
+include(":app")
+ 
